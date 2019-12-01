9f73a144515ae46ca0dcb3b5f788ef424b6ddd84
Stephen Mizell <smizell@gmail.com>
2019-12-01 11:22:02 -0600

    Clean up code a bit

--- 2019/01/code.rkt
+++ 2019/01/code.rkt
@@ -3,8 +3,13 @@
 (require threading
          rackunit)
 
-(define (calc-fuel m)
-  (~> m (/ 3) (floor) (- 2)))
+(define inputs
+  (~>> "./input.txt"
+       (file->lines)
+       (map string->number)))
+
+(define (calc-fuel mass)
+  (~> mass (/ 3) (floor) (- 2)))
 
 (module+ test
   (check-equal? (calc-fuel 12) 2)
@@ -12,19 +17,19 @@
   (check-equal? (calc-fuel 1969) 654)
   (check-equal? (calc-fuel 100756) 33583))
 
-(define (calc-module-fuel m)
-  (let ([f (calc-fuel m)])
+; Calculate fuel recursively
+; Part 1 didn't account for mass of fuel so we add
+; fuel until we get to 0
+(define (calc-fuel* mass)
+  (let ([fuel (calc-fuel mass)])
     (cond
-      [(negative? f) 0]
-      [else (+ f (calc-module-fuel f))])))
+      [(negative? fuel) 0]
+      [else (+ fuel (calc-fuel* fuel))])))
 
 (module+ test
-  (check-equal? (calc-module-fuel 1969) 966))
+  (check-equal? (calc-fuel* 1969) 966))
 
-(define inputs
-  (~>> "./input.txt"
-       (file->lines)
-       (map string->number)))
+; Answers
 
 (define part1
   (~>> inputs
@@ -33,5 +38,5 @@
 
 (define part2
   (~>> inputs
-       (map calc-module-fuel)
+       (map calc-fuel*)
        (apply +)))
