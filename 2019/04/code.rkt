d5df3943ad60e7baeaa9ecf94535bb0878c7c007
Stephen Mizell <smizell@gmail.com>
2019-12-04 00:40:44 -0600

    Part 2

--- 2019/04/code.rkt
+++ 2019/04/code.rkt
@@ -4,12 +4,13 @@
 
 (module+ test (require rackunit))
 
+(define (repeating-regex s)
+  (regexp-match* #px"(\\d+)\\1+" s))
+
 (define (has-repeating? n)
   (let* ([s (number->string n)]
-         [m (regexp-match #px"(\\d+)\\1" s)])
-    (cond
-      [(eq? m #f) #f]
-      [(not (empty? m)) #t])))
+         [m (repeating-regex s)])
+    (not (empty? m))))
 
 (module+ test
   (check-equal? (has-repeating? 111111) #t)
@@ -17,6 +18,7 @@
   (check-equal? (has-repeating? 111123) #t)
   (check-equal? (has-repeating? 123789) #f))
 
+; This is ugly :)
 (define (number->list n)
   (~>> n
        (number->string)
@@ -41,3 +43,34 @@
 
 (define (part1)
   (length (find-matches)))
+
+(define (any? fn l)
+  (cond
+    [(empty? l) #f]
+    [else (or (fn (first l)) (any? fn (rest l)))]))
+
+(module+ test
+  (check-equal? (any? (lambda (v) (eq? v #t)) '(#t #f)) #t)
+  (check-equal? (any? (lambda (v) (eq? v #t)) '(#f #f)) #f))
+
+(define (only-two? s)
+  (eq? (string-length s) 2))
+
+(module+ test
+  (check-equal? (only-two? "aa") #t))
+
+(define (any-only-two? l)
+  (any? only-two? l))
+
+(module+ test
+  (check-equal? (any-only-two? '("rr" "ee")) #t)
+  (check-equal? (any-only-two? '("rre" "ee")) #t)
+  (check-equal? (any-only-two? '("rre" "eee")) #f))
+
+(define (part2)
+  (~>> (find-matches)
+       (map number->string)
+       (map repeating-regex)
+       (filter any-only-two?)
+       (length)))
+
